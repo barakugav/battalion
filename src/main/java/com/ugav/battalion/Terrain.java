ef4eee84cd6b9087cd00317fe100b2044dc90e75
Barak Ugav <barakugav@gmail.com>
2020-10-11 21:22:50 +0300

    Terrain [New Feature]: static final field for each terrain type

--- src/main/java/com/ugav/battalion/Terrain.java
+++ src/main/java/com/ugav/battalion/Terrain.java
@@ -2,19 +2,35 @@ package com.ugav.battalion;
 
 public class Terrain {
 
+    static final FlatLand FLAT_LAND = new FlatLand();
+    static final ClearWater CLEAR_WATER = new ClearWater();
+    static final Mountain MOUNTAIN = new Mountain();
+
     static class Land extends Terrain {
+
+	private Land() {
+	}
 
     }
 
     static class FlatLand extends Land {
 
+	private FlatLand() {
+	}
+
     }
 
     static class Mountain extends Land {
 
+	private Mountain() {
+	}
+
     }
 
     static class ClearWater extends Terrain {
+
+	private ClearWater() {
+	}
 
     }
 
