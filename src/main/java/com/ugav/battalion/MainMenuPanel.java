eb3fce502bbbccea1893080979d8e692855c74c3
Barak Ugav <barakugav@gmail.com>
2020-10-11 23:44:48 +0300

    Main Menu [New Feautre] levels buttons

--- src/main/java/com/ugav/battalion/MainMenuPanel.java
+++ src/main/java/com/ugav/battalion/MainMenuPanel.java
@@ -3,6 +3,7 @@ package com.ugav.battalion;
 import java.awt.GridLayout;
 import java.util.Objects;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 import com.ugav.battalion.level.HardcodedLevel1;
@@ -18,11 +19,34 @@ public class MainMenuPanel extends JPanel {
 
 	int levelCount = data.getLevelCount();
 	setLayout(new GridLayout(levelCount, 1));
+
+	for (int levelIdx = 0; levelIdx < data.getLevelCount(); levelCount++)
+	    add(new LevelButton(levelIdx));
     }
 
     private void selectLevel(int levelIdx) {
-	// gameFrame.getGamePlane().setL
-	// TODO
+	Game game = new Game(data.getLevel(levelIdx));
+	game.start();
+	gameFrame.getGamePlane().setGame(game);
+	gameFrame.displayGame();
+    }
+
+    private class LevelButton extends JButton {
+
+	private final int levelIdx;
+
+	LevelButton(int levelIdx) {
+	    if (levelIdx < 0)
+		throw new IllegalArgumentException();
+	    this.levelIdx = levelIdx;
+
+	    setText(String.format("Level %2d", Integer.valueOf(levelIdx)));
+	    setSize(100, 100);
+	    addActionListener(e -> {
+		selectLevel(this.levelIdx);
+	    });
+	}
+
     }
 
     private static class Data {
